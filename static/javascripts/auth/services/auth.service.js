8d4f10007530faabbd5120168f64454b942ed80f
James Brewer <james@brwr.org>
2014-09-21 13:51:09 -0700

    Create logout function for Auth service.

--- static/javascripts/auth/services/auth.service.js
+++ static/javascripts/auth/services/auth.service.js
@@ -1,3 +1,37 @@
 window.angular.module('application.auth.services').
-	service('Auth', function () {
+	service('Auth', function ($http, $q, $window) {
+		var getToken = function () {
+			return JSON.parse($window.localStorage.getItem('token'));
+		};
+
+		var setToken = function (token) {
+			$window.localStorage.setItem('token', token);
+		};
+
+		var deleteToken = function () {
+			$window.localStorage.removeItem('token');
+		};
+
+		var Auth = {
+			login: function (username, password) {
+				var deferred = $q.defer();
+
+				return deferred.promise;
+			},
+
+			logout: function () {
+				var deferred = $q.defer();
+
+				$http.post('/api/v1/auth/login').
+					success(function (response, status, headers, config) {
+						deleteToken();
+
+						deferred.resolve(response, status, headers, config);
+					});
+
+				return deferred.promise;
+			}
+		};
+
+		return Auth;
 	});
