c0eac7e066838702d33bbc1ddd4f93e175c8359f
James Brewer <james@brwr.org>
2014-09-21 13:15:00 -0700

    Fix typo.

--- django_angular_token_auth/urls.py
+++ django_angular_token_auth/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^', include(router.urls)),
-    url(r'^auth/api/', include('rest_framework.urls', namespane='rest_framework')),
+    url(r'^auth/api/', include('rest_framework.urls', namespace='rest_framework')),
 )
