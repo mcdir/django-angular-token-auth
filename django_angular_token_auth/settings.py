5a97cd72ee67c47ffb496e60bd1d4ae668d72fa7
James Brewer <james@brwr.org>
2014-09-21 12:03:08 -0700

    Move auth/ app to authentication/ and update settings.

--- django_angular_token_auth/settings.py
+++ django_angular_token_auth/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'authentication',
 )
 
 MIDDLEWARE_CLASSES = (
