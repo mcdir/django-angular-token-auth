305c0021b8239bd042a4af05eb40931f70e79ede
James Brewer <james@brwr.org>
2014-09-21 16:26:58 -0700

    Redirect to login page if token is expired.

--- static/javascripts/auth/interceptors/auth.interceptor.js
+++ static/javascripts/auth/interceptors/auth.interceptor.js
@@ -1,5 +1,5 @@
 window.angular.module('application.auth.interceptors')
-	.service('AuthInterceptor', function ($injector) {
+	.service('AuthInterceptor', function ($injector, $location) {
 		var AuthInterceptor = {
 			request: function (config) {
 				var Auth = $injector.get('Auth');
@@ -10,6 +10,16 @@ window.angular.module('application.auth.interceptors')
 				}
 
 				return config;
+			},
+
+			responseError: function (response) {
+				if (response.status === 403) {
+					if (response.data.detail === 'Signature has expired.') {
+						$location.path('/login');
+					}
+				}
+
+				return response;
 			}
 		};
 
