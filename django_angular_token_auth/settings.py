fa5fe3698bb17debd88a761b2f6bffdca703b1b0
James Brewer <james@brwr.org>
2014-09-21 14:08:03 -0700

    Add JSONWebTokenAuthentication to DEFAULT_AUTHENTICATION_CLASSES setting.

--- django_angular_token_auth/settings.py
+++ django_angular_token_auth/settings.py
@@ -93,6 +93,13 @@ TEMPLATE_DIRS = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSIONS_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
     'PAGINATE_BY': 10
 }
