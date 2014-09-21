759d6c87c1717dfc39b4526dd1454ffb637611bc
James Brewer <james@brwr.org>
2014-09-21 13:09:06 -0700

    Add viewsets for User and Group.

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
 
-# Create your views here.
+from rest_framework import viewsets
+
+from authentication.serializers import GroupSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	'''
+	API endpoint that allows users to be viewed or edited.
+	'''
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+	'''
+	API endpoint that allows groups to be viewed or edited.
+	'''
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
