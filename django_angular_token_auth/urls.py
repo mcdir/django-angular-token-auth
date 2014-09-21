8bb125ae50a7c121fa60c673be05928aa3519db1
James Brewer <james@brwr.org>
2014-09-21 13:12:26 -0700

    Add URLs for User and Group views.

--- django_angular_token_auth/urls.py
+++ django_angular_token_auth/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_angular_token_auth.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from rest_framework import routers
 
+from authentication.views import GroupViewSet, UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^', include(router.urls)),
+    url(r'^auth/api/', include('rest_framework.urls', namespane='rest_framework')),
 )
