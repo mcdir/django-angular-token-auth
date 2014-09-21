aadc6c563d77244c6e28c4dc0867e06b32bc88af
James Brewer <james@brwr.org>
2014-09-21 13:53:04 -0700

    Move auth for API to /api/v1/auth/api/ and add placeholder routes for login, logout, and registration.

--- django_angular_token_auth/urls.py
+++ django_angular_token_auth/urls.py
@@ -13,7 +13,14 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v1/auth/api/', include(
+    	'rest_framework.urls', namespace='rest_framework'
+    )),
+
+    # Placeholder routes
+    url(r'^api/v1/auth/login/', TemplateView.as_view()),
+    url(r'^api/v1/auth/logout/', TemplateView.as_view()),
+    url(r'^api/v1/auth/register/', TemplateView.as_view()),
 
     url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 )
