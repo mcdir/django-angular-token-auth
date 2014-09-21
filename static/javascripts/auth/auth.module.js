35a30093195aac6029462b9cbc8c18fee3b50b20
James Brewer <james@brwr.org>
2014-09-20 23:59:55 -0700

    Bootstrap Auth service and controllers.

---
+++ static/javascripts/auth/auth.module.js
@@ -0,0 +1,7 @@
+window.angular.module('application.auth', [
+	'application.auth.controllers',
+	'application.auth.services'
+]);
+
+window.angular.module('application.auth.controllers', []);
+window.angular.module('application.auth.services', []);
---
+++ static/javascripts/auth/services/auth.service.js
@@ -0,0 +1,1 @@
+window.angular.module('application.auth.services');
