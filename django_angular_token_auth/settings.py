0d5af4d09332efeefe498c08843434019877b017
James Brewer <james@brwr.org>
2014-09-21 16:20:43 -0700

    JWT is good for two weeks.

--- django_angular_token_auth/settings.py
+++ django_angular_token_auth/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=6),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14),
     'JWT_ALLOW_REFRESH': True,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=14)
 }
