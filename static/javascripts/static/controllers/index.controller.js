f19cb88a04dc0e3d8e739bb16156b82674da9189
James Brewer <james@brwr.org>
2014-09-25 23:21:39 -0700

    Refactor index.

--- static/javascripts/static/controllers/index.controller.js
+++ static/javascripts/static/controllers/index.controller.js
@@ -1,6 +1,6 @@
 window.angular.module('application.static.controllers')
 	.controller('IndexController', function ($scope, Users) {
 		Users.all().then(function (response, status, headers, config) {
-			$scope.users = response;
-		})
+			$scope.users = response.results;
+		});
 	});
