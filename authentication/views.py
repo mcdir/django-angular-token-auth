7510ce5d7bedbf1adb5536cf7b32eb0260011005
James Brewer <james@brwr.org>
2014-09-25 22:43:19 -0700

    ListAPIView is in `rest_framework.generics` module.

--- authentication/views.py
+++ authentication/views.py
@@ -1,26 +1,9 @@
 from django.contrib.auth.models import User
 
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from authentication.serializers import UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-	'''
-	API endpoint that allows users to be viewed or edited.
-	'''
+class UserListAPIView(generics.ListAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-
-class LoginAPIView(APIView):
-	def post(self, request, format=None):
-		return Response({ 'login': True })
-
-class LogoutAPIView(APIView):
-	def post(self, request, format=None):
-		return Response({ 'logout': True })
-
-class RegisterAPIView(APIView):
-	def post(self, request, format=None):
-		return Response({ 'register': True })
