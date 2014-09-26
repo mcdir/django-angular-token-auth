0224d88941410e258fed1580b0a74143e108f28a
James Brewer <james@brwr.org>
2014-09-25 23:22:03 -0700

    Add login method/template.

--- static/javascripts/auth/controllers/login.controller.js
+++ static/javascripts/auth/controllers/login.controller.js
@@ -1,4 +1,16 @@
 window.angular.module('application.auth.controllers')
-	.controller('LoginController', function ($scope) {
-	
+	.controller('LoginController', function ($scope, Auth) {
+		$scope.user = {};
+
+		$scope.login = function () {
+			Auth.login($scope.user.username, $scope.user.password).then(
+				function (response, status, headers, config) {
+					window.location = '/';
+				},
+				function (response, status, headers, config) {
+					console.error('Error loggin in.');
+				}	
+			);
+		};
+
 	});
