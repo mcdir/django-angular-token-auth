af9425180612a0baf5eba6ba45dd838ffc07f376
James Brewer <james@brwr.org>
2014-09-25 22:44:02 -0700

    Update routes.

--- django_angular_token_auth/urls.py
+++ django_angular_token_auth/urls.py
@@ -1,27 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.views.generic import TemplateView
 
-from rest_framework import routers
-
-from authentication.views import LoginAPIView, LogoutAPIView, \
-                                 RegisterAPIView, UserViewSet
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from authentication.views import UserListAPIView
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/auth/api/', include(
-    	'rest_framework.urls', namespace='rest_framework'
-    )),
-
     url(r'^api/v1/auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^api/v1/auth/refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
-    url(r'^api/v1/auth/logout/', LogoutAPIView.as_view()),
-    url(r'^api/v1/auth/register/', RegisterAPIView.as_view()),
+	url(r'^api/v1/users', UserListAPIView.as_view()),
 
     url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 )
