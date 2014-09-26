37e01e501e5a95d9794cd113eebec2abc7818d39
James Brewer <james@brwr.org>
2014-09-25 23:18:50 -0700

    Remove extra space.

--- django_angular_token_auth/urls.py
+++ django_angular_token_auth/urls.py
@@ -5,7 +5,7 @@ from authentication.views import UserListAPIView
 
 urlpatterns = patterns('',
     url(r'^api/v1/auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
-	url(r'^api/v1/users', UserListAPIView.as_view()),
+    url(r'^api/v1/users', UserListAPIView.as_view()),
 
     url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 )
